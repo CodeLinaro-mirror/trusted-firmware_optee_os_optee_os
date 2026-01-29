42aafac5a2632621b84375e5d11611a3c7c5539d
Viswa Kumar Nambirajan <vnambira@qti.qualcomm.com>
2026-01-29 11:55:56 +0530

    plat-qcom: ipq52xx: Add RESET_SGI interrupt
    This patch adds RESET_SGI interrupt configuration
    to deleagte and handle the interrupt in EL3.
    
    Change-Id: I2819284b99c75d3e16f5c65affe3998de3bdc3a2
    Signed-off-by: Viswa Kumar Nambirajan <vnambira@qti.qualcomm.com>
    Signed-off-by: Nitheesh Sekar <nsekar@qti.qualcomm.com>

--- core/arch/arm/plat-qcom/ipq/ipq52xx/gicv2_config.h
+++ core/arch/arm/plat-qcom/ipq/ipq52xx/gicv2_config.h
@@ -12,6 +12,7 @@
 #define SEC_WDOG_BARK_INT_ID		0x23u
 #define NON_SEC_WDOG_BITE_INT_ID	0x136u
 #define XPU_VIOLATION_INT_ID		0xB1u
+#define RESET_SGI_INT_ID		0xFu
 
 /*
  * List of interrupt IDs to be delegated to EL3 from SEL1.
@@ -21,6 +22,7 @@ static const uint32_t el3_delegated_interrupts[] = {
 	SEC_WDOG_BARK_INT_ID,
 	NON_SEC_WDOG_BITE_INT_ID,
 	XPU_VIOLATION_INT_ID,
+	RESET_SGI_INT_ID
 };
 
 #endif /* GICV2_CONFIG_H */
