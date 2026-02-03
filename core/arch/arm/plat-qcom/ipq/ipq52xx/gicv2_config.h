803ebaeb78a3650a6e973a8147bc3730bda51a98
Viswa Kumar Nambirajan <vnambira@qti.qualcomm.com>
2026-02-03 15:12:44 +0530

    plat-qcom: ipq52xx: Add xPU violation interrupt handling
    Add support for handling xPU (eXtended Protection Unit) violation
    interrupts in the ipq52xx platform by defining the XPU_VIOLATION_INT_ID
    and adding it to the list of interrupts delegated to EL3 from SEL1.
    
    Change-Id: I5cc79cd2918b3a9ca8aba0dd36713ee4e440fa40
    Signed-off-by: Viswa Kumar Nambirajan <vnambira@qti.qualcomm.com>

--- core/arch/arm/plat-qcom/ipq/ipq52xx/gicv2_config.h
+++ core/arch/arm/plat-qcom/ipq/ipq52xx/gicv2_config.h
@@ -11,6 +11,7 @@
 /* Interrupt IDs for EL3 delegation */
 #define SEC_WDOG_BARK_INT_ID		0x23u
 #define NON_SEC_WDOG_BITE_INT_ID	0x136u
+#define XPU_VIOLATION_INT_ID		0xB1u
 
 /*
  * List of interrupt IDs to be delegated to EL3 from SEL1.
@@ -18,7 +19,8 @@
  */
 static const uint32_t el3_delegated_interrupts[] = {
 	SEC_WDOG_BARK_INT_ID,
-	NON_SEC_WDOG_BITE_INT_ID
+	NON_SEC_WDOG_BITE_INT_ID,
+	XPU_VIOLATION_INT_ID,
 };
 
 #endif /* GICV2_CONFIG_H */
