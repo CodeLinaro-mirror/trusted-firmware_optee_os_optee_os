OP-TEE/optee_os

core: drivers: introduce Qualcomm GENI UART driver
Introduce a driver for the GENI UART found on modern Qualcomm platforms.

Signed-off-by: Casey Connolly <casey.connolly@linaro.org>
Signed-off-by: Rouven Czerwinski <rouven.czerwinski@linaro.org>

core/drivers/sub.mk
@@ -11,6 +11,7 @@ srcs-$(CFG_PL022) += pl022_spi.c
 srcs-$(CFG_SP805_WDT) += sp805_wdt.c
 srcs-$(CFG_8250_UART) += serial8250_uart.c
 srcs-$(CFG_16550_UART) += ns16550.c
+srcs-$(CFG_GENI_UART) += geni_uart.c
 srcs-$(CFG_IMX_SNVS) += imx_snvs.c
 srcs-$(CFG_IMX_UART) += imx_uart.c
 srcs-$(CFG_IMX_I2C) += imx_i2c.c

CodeLinaro-mirror/trusted-firmware_optee_os_optee_os

Acked-by: Jerome Forissier <jerome.forissier@linaro.org>
Acked-by: Jens Wiklander <jens.wiklander@linaro.org>
Change-Id: I958e41d49ef0ca9b4d88afa809c762d784f7c7c4
[SG: cleaned up the driver]
Signed-off-by: Sumit Garg <sumit.garg@oss.qualcomm.com>
(cherry picked from commit cdd2fe13991b335d22f74be6fe412f90eb796497)

core/drivers/sub.mk
@@ -11,6 +11,7 @@ srcs-$(CFG_PL022) += pl022_spi.c
 srcs-$(CFG_SP805_WDT) += sp805_wdt.c
 srcs-$(CFG_8250_UART) += serial8250_uart.c
 srcs-$(CFG_16550_UART) += ns16550.c
+srcs-$(CFG_QCOM_GENI_UART) += qcom_geni_uart.c
 srcs-$(CFG_IMX_SNVS) += imx_snvs.c
 srcs-$(CFG_IMX_UART) += imx_uart.c
 srcs-$(CFG_IMX_I2C) += imx_i2c.c